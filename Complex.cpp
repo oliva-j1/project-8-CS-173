d22d3772523f4dc5b391db86a95c9ab6436f69a9
bencoverdale <coverd_b1@denison.edu>
2026-03-27 09:47:59 -0400

    First 3 classes

--- Complex.cpp
+++ Complex.cpp
@@ -15,15 +15,32 @@ using namespace std;
 //========================================================
 // default constructor
 //========================================================
-
+    void defaultConstructor(){
+        Complex c;
+        c.setReal(0);
+        c.setImag(0);
+    }
 //========================================================
 // copy constructor
 //========================================================
+    void copyConstructor(const Complex& c){
+       // Fetches real and imaginary number from existing complex number
+        realnum = c.getReal;
+        imagnum = c.getImag;
+        // This part copies the fetched values to the new complex number
+        Complex newC;
+        newC.setReal(realnum);
+        newC.setImag(imagnum);
 
+    }
 //========================================================
 // Complex(a,b) constructor
 //========================================================
-
+    void complexConstructor(double real, double imag){
+        Complex c;
+        c.setReal(real);
+        c.setImag(imag);
+    }
 //========================================================
 // destructor
 //========================================================
@@ -51,7 +68,22 @@ using namespace std;
 //========================================================
 // addition operators
 //========================================================
-
+    void additionOperators(Complex& c1, Complex& c2){
+        // Fetch real and imaginary numbers from c1
+        double realnum1 = c1.getReal();
+        double imagnum1 = c1.getImag();
+        // Fetch real and imaginary numbers from c2
+        double realnum2 = c2.getReal();
+        double imagnum2 = c2.GetImag();
+        // Add real numbers
+        double realsum = realnum1 + realnum2;
+        // Add imaginary numbers
+        double imagsum = imagnum1 + imagnum2;
+        // Create sum structure
+        Complex sum
+        sum.setReal(realsum);
+        sum.setImag(imagsum);
+    }
 //========================================================
 // subtraction operators
 //========================================================
